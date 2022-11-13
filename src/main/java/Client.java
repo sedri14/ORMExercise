8a6cebc22b6474605ed156a332be34b8f7f15b78
Sedri14 <sedri14@gmail.com>
2022-11-13 23:01:21 +0200

    added findOne method for user

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
+
+        //findAll
         List<User> users = MysqlCon.executeQuery("select * from user");
         users.forEach(user -> System.out.println(user));
+
+        //findOne
+        User user = MysqlCon.findOne(2);
+        System.out.println(user);
     }
 }
--- src/main/java/MysqlCon.java
+++ src/main/java/MysqlCon.java
@@ -9,6 +9,29 @@ import java.util.List;
 
 class MysqlCon {
 
+    public static User findOne(int id) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/orm", "root", "Sharoni123");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(String.format("select * from user where id=%d", id));
+            User user = new User();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+                user.setName(rs.getString(2));
+                user.setAge(rs.getInt(3));
+            } else {
+
+            }
+            con.close();
+            return user;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
     public static List<User> executeQuery(String query) {
 
         try {
